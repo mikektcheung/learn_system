81a3f595c0ce76c994cdacb49954d64edf32372e
mikektcheung <mikektcheung@gmail.com>
2021-06-06 01:49:23 +0000

    update

--- lms/lms/settings.py
+++ lms/lms/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-#Do not commit to github
+#Do not commit to github 2021
 
 from pathlib import Path
 
